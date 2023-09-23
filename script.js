ab4f1b449e93e69a058ed3a45a682ff9989fd396
matteocampisciano <123169062+matteocampisciano@users.noreply.github.com>
2023-09-23 18:30:39 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-$ function
-  {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset 
-        }
